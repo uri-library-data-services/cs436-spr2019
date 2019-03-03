262479e8749098b366fe91a67fe23a00fa856e99
Harrison Dekker <hdekker@uri.edu>
2019-03-03 14:24:01 -0500

    Add cleaning steps for remaining tables

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -1,3 +1,10 @@
+import re
+import csv
+import pandas as pd
+import json
+
+
+#
 # Extract author data from "authors" file
 #
 # Inputs:
@@ -5,14 +12,6 @@
 #
 # Outputs:
 # ../data_output/authors.csv
-
-import csv
-import pandas as pd
-import json
-
-
-#
-# read authors
 #
 
 doc = "../data_output/ai-authors.tsv"
@@ -39,8 +38,6 @@ for author in authors:
 with open("../data_output/authors.csv", 'w', encoding='utf-8') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerows(author_ids)
-
-
 #
 # editions
 #
@@ -50,9 +47,13 @@ with open("../data_output/authors.csv", 'w', encoding='utf-8') as myfile:
 #
 # Outputs:
 # ../data_output/editions_works.csv
-# ../data_output/editions.csv
+# ../data_output/edition_publisher.csv
+# ../data_output/publishers.csv
+# ../data_output/places.csv
 
-
+#
+# Save:
+# editionpub = {editionid:{publisherid:value,pubyear:value,pubplace:value}}
 doc = "../data_output/ai-editions.tsv"
 df = pd.read_csv(doc, sep='\t', header=None)
 
@@ -62,14 +63,10 @@ df.columns = ['type', 'path', 'revisions', 'timestamp', 'details']
 # get 'work_id' from 'path'
 df['edition_id'] = df.path.str[7:]
 
-# create a list of lists containing only the id and json data
+edition_work = [["editionid", "workid"]] # edition_id, work_id pairs
+edition_tbl = [["id", "title", "numpages", "ISBN10", "ISBN13", "physfmt", "pubdate"]]
 editions_master = df[['edition_id', 'details']].values.tolist()
-#
-# PASS 1
-#
-
-edition_publisher = [["edition_id", "publisher_id"]]
-publisher_place = [["pub_id", "place_id"]]
+edition_publisher = [["editionid", "publisherid", "pubyear","pubplace"]]
 publisher_ids = {"NULL": "0"} # pub_name => pub_id
 place_ids = {"NULL": "0"} # place_name => place_id
 pub_id = 1
@@ -103,31 +100,23 @@ for edition in editions_master:
                 pubyear = ""
         else:
             pubyear = ""
+        edition_publisher.append([edition[0],
+                          publisher_ids[publisher],
+                          pubyear,
+                          place_ids[pub_place]]) 
 
-
-#
-# PASS 2
-#
-edition_work = ["editionid", "workid"] # edition_id, work_id pairs
-edition_tbl = [["id", "title", "numpages", "ISBN10", "ISBN13", "physfmt", "pubdate"]]
-# also grab pubplace and pubyear in lists to use later
-for edition in editions_master:
-    details = json.loads(edition[1])
-    # get work_ids -- lookup table
     edition_work.append([edition[0], details['works'][0]['key'][7:]])
     # get number of pages
     if 'number_of_pages' in details.keys():
         number_of_pages = details["number_of_pages"]
     else:
         number_of_pages = "NULL"
-    # get isbn10 (Note: occasionally there are multiple ISBN's so to simplify we 
-    # only select the first one)
+    # get isbn10 
     if 'isbn_10' in details.keys():
         isbn_10 = details["isbn_10"][0]
     else:
         isbn_10 = "NULL"
-    # get isbn13 (Note: occasionally there are multiple ISBN's so to simplify we 
-    # only select the first one)
+    # get isbn13 
     if 'isbn_13' in details.keys():
         isbn_13 = details["isbn_13"][0]
     else:
@@ -142,19 +131,26 @@ for edition in editions_master:
         physical_format = details["physical_format"]
     else:
         physical_format = "NULL"
-    # get publication date
     if 'publish_date' in details.keys():
         pub_date = details['publish_date']
     else:
         pub_date = "NULL"
-    # get publication place
-    if 'publish_date' in details.keys():
-        pub_date = details['publish_date']
-    else:
-        pub_date = "NULL"
+    edition_tbl.append([edition[0], title, number_of_pages, isbn_10, isbn_13, physical_format, pub_date])
 
-    edition_tbl.append([edition[0], number_of_pages, isbn_10, isbn_13, title, physical_format, pub_date])
+with open("../data_output/edition_publisher.csv", 'w', encoding='utf-8', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(edition_publisher)
 
+with open("../data_output/publishers.csv", 'w', encoding='utf-8', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(['id', 'name'])
+    for key in publisher_ids.keys():
+        wr.writerow([publisher_ids[key], key])
+with open("../data_output/places.csv", 'w', encoding='utf-8', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(['id', 'name'])
+    for key in place_ids.keys():
+        wr.writerow([place_ids[key], key])
 with open("../data_output/edition_work.csv", 'w', encoding='utf-8', newline='') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerows(edition_work)
@@ -162,3 +158,98 @@ with open("../data_output/edition_work.csv", 'w', encoding='utf-8', newline='') 
 with open("../data_output/editions.csv", 'w', encoding='utf-8', newline='') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerows(edition_tbl)
+#quit()
+
+
+# Process 'works' data
+# Inputs:
+# ../data_output/artificial_intelligence_works.txt
+
+# Outputs:
+# ../data_output/works.csv
+# ../data_output/author_works.csv
+# ../data_output/works_subject.csv
+# ../data_output/subjects.csv
+
+### Grab works data
+doc = "../data_output/artificial_intelligence_works.txt"
+df = pd.read_csv(doc, sep='\t', header=None)
+
+# add column names
+df.columns = ['type', 'path', 'revisions', 'timestamp', 'details']
+
+# get 'work_id' from 'path'
+df['work_id'] = df.path.str[7:]
+
+# create a list containing only the id and json data
+works = df[['work_id', 'details']].values.tolist()
+
+# initialize some lists to hold the data that will get saved in csv files
+titles = [["id","title"]] # work_id and title
+work_authors = [["workid","authorid"]] # work_id and author_id
+subjects = [] # temporary storage for work_id and subject
+subject_tbl = [["id","subject"]] # subject_id
+work_subject = [["workid","subjectid"]] #work_id and subject_id
+
+# Loop through 'works' and pull out the pieces of data we want in the database
+for work in works:
+    details = json.loads(work[1])
+
+    # append title to list
+    titles.append([work[0], details["title"]])
+
+    # append authors to list - not all works have authors!
+    try:
+        for a in details["authors"]:
+            work_authors.append([work[0], a["author"]["key"][9:]])
+    except:
+        # print(json.dumps(details))
+        continue
+
+    # append subjects to list
+    try:
+        for s in details["subjects"]:
+            subjects.append([work[0], s])
+    except:
+        # print(json.dumps(details))
+        continue
+
+# Create a set from the subjects list
+unique_subjects = set()
+for subject in subjects:
+    unique_subjects.add(subject[1])
+
+# Generate a subject id for each subject in the set.
+n = 1
+for u in sorted(unique_subjects):
+    subject_tbl.append([str(n), u])
+    n += 1
+
+# Create a dictionary to allow subject lookups
+subject_id = {}
+for s in subject_tbl:
+    subject_id[s[1]] = s[0]
+
+# Create a list of lists containing pairs of subject id's and work id's
+
+for s in subjects:
+    work_subject.append([s[0], subject_id[s[1]]])
+
+# Write "titles" to csv
+with open("../data_output/works.csv", 'w', encoding='utf-8', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(titles)
+
+# Write "work_authors" to csv
+with open("../data_output/author_works.csv", 'w', encoding='utf-8', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(work_authors)
+
+with open("../data_output/work_subjects.csv", 'w', encoding='utf-8', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(work_subject)
+
+with open("../data_output/subjects.csv", 'w', encoding='utf-8', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(subject_tbl)
+
